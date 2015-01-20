8a849ecd2515a88dcb3672a92446043f7edac081
Lingchao Xin <douglarek@gmail.com>
2015-01-20 23:57:56 +0800

    LICENSE updated

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject cist "0.1.0-SNAPSHOT"
   :description "A Clojure command-line wrapper with tentacles library to access GitHub Gist"
   :url "https://github.com/douglarek/cist"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Copyright (c) 2015, Lingchao Xin"
+            :url "https://raw.githubusercontent.com/douglarek/cist/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.6.0"] [tentacles "0.3.0"] [org.clojure/tools.cli "0.3.1"]]
   :main cist.core
   :aot [cist.core]
