8f2b47978b4b63c67aa250adac6b21563c9102aa
Lingchao Xin <douglarek@gmail.com>
2016-11-20 23:57:15 +0800

    Cist 1.0.2 snapshot starts

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cist "1.0.1"
+(defproject cist "1.0.2-SNAPSHOT"
   :description "A Clojure command-line wrapper with tentacles library to access GitHub Gist"
   :url "https://github.com/douglarek/cist"
   :license {:name "Copyright (c) 2016, Lingchao Xin"
