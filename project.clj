ea0bac6929ddb94af484bfc25ea1163c27fb0d0d
Lingchao Xin <douglarek@gmail.com>
2016-11-20 23:09:27 +0800

    Update clj-http to 3.4.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/douglarek/cist"
   :license {:name "Copyright (c) 2016, Lingchao Xin"
             :url "https://raw.githubusercontent.com/douglarek/cist/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.8.0"] [tentacles "0.5.1"] [org.clojure/tools.cli "0.3.5"] [clj-http "2.1.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"] [tentacles "0.5.1"] [org.clojure/tools.cli "0.3.5"] [clj-http "3.4.1"]]
   :main cist.core
   :omit-source true
   :aot [cist.core]
