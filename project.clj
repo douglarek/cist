4c40eb26f4d3b2107c01283372e9490fa34ef7d3
Lingchao Xin <douglarek@gmail.com>
2017-11-10 10:54:21 +0800

    Bump version to 1.0.2

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject cist "1.0.2-SNAPSHOT"
+(defproject cist "1.0.2"
   :description "A Clojure command-line wrapper with tentacles library to access GitHub Gist"
   :url "https://github.com/douglarek/cist"
-  :license {:name "Copyright (c) 2016, Lingchao Xin"
+  :license {:name "Copyright (c) 2017, Lingchao Xin"
             :url "https://raw.githubusercontent.com/douglarek/cist/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.8.0"] [tentacles "0.5.1"] [org.clojure/tools.cli "0.3.5"] [clj-http "3.4.1"]]
   :main cist.core
   :omit-source true
-  :aot [cist.core]
-  )
+  :aot [cist.core])
